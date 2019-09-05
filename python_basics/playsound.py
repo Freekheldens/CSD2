9071581648120c937a3fb8eb5f597a51d48c0fdd
Freekheldens <freekheldens@hotmail.com>
2019-09-05 14:30:30 +0200

    improved playsound.py

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -2,12 +2,15 @@ import simpleaudio as sa
 
 wave_obj = sa.WaveObject.from_wave_file("/Users/freek/School/Jaar 2/CSD/Git/CSD2/python_basics/plingel.wav")
 
-play_times = int (input("How many times would you like to hear the sample? "))
+play_times = input("How many times would you like to hear the sample? ")
 
-i = 0
-while i < play_times:
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
-    i += 1
+if str.isdigit(play_times):
+    i = 0
+    while i < int(play_times):
+        play_obj = wave_obj.play()
+        play_obj.wait_done()
+        i += 1
+    else:
+        print("All done!")
 else:
-    print("All done!")
+    print("Wrong input!")
