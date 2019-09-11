b67230d2c4f92a447a8c86a3355648c4954357ce
Freekheldens <freekheldens@hotmail.com>
2019-09-11 19:20:58 +0200

    Added comments

--- python_basics/helloworld.py
+++ python_basics/helloworld.py
@@ -1,7 +1,7 @@
-username = input("Please enter your name: ")
-age = int (input("Please enter your age: "))
+username = input("Please enter your name: ") #input je naam
+age = int (input("Please enter your age: ")) #input je leeftijd
 
-if age < 18:
-  print("Sorry you are to young :(")
+if age < 18:                                 # kijkt of je input leeftijd kleiner is als 18
+  print("Sorry you are to young :(")         # dit wordt uitgevoerd als de leeftijd kleiner is dan 18
 else:
-  print("Hello " + username + "!")
+  print("Hello " + username + "!")           # dit wordt uitgevoerd als leeftijd gelijk is of groter is dan 18
