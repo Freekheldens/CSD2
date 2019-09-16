6a06d12d42f805682e6426ec647b448d18d2031d
Freekheldens <freekheldens@hotmail.com>
2019-09-16 20:38:05 +0200

    Added comments

--- python_basics/rythmic_playsound.py
+++ python_basics/rythmic_playsound.py
@@ -1,20 +1,24 @@
-import simpleaudio as sa
-import time
+import simpleaudio as sa                                                                                        #import simpleaudio module
+import time                                                                                                     #import time module
 
-wave_obj = sa.WaveObject.from_wave_file("/Users/freek/School/Jaar 2/CSD/Git/CSD2/python_basics/RS01.wav")
+wave_obj = sa.WaveObject.from_wave_file("/Users/freek/School/Jaar 2/CSD/Git/CSD2/python_basics/RS01.wav")       #path naar audiofile
+
+list_rythm_values = []                                                                                          #lege lijst waar de nootlengtes in worden gezet
 
 play_times = int(input("How many times would you like to hear the sample? "))
 
-list_rythm_values = []
-
-for times in range(play_times):
-    rythm_values = (input("Please enter a number that represents the lenght of a note: "))
+for times in range(play_times):                                                                                 #loop om de lijst te vullen
+    rythm_values = (input("Give each playback time its own length, 1 = quarter note 0.5 = 8th note etc."))
     list_rythm_values.append(rythm_values)
 
 bpm = input("Please enter a BPM: ")
-bpm = (60000/float(bpm))
+if(bpm.isdigit()):
+    bpm = (60000/float(bpm))                                                                                    #rekent bpm om naar milliseconde
+else:
+    print("Please enter a number!")
+    exit()
 
-for rythms in list_rythm_values:
+for rythms in list_rythm_values:                                                                                #loop die de sample afspeelt met de delay's
     rythms = float(rythms)
     play_obj = wave_obj.play()
     print(str(rythms*bpm) + " ms")
