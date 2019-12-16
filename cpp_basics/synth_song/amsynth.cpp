bdffc86172d74b04de09a5ae978ab2dcece45f80
Freekheldens <freekheldens@hotmail.com>
2019-12-16 16:19:48 +0100

    made loop for melody generatot

--- cpp_basics/synth_song/amsynth.cpp
+++ cpp_basics/synth_song/amsynth.cpp
@@ -1,4 +1,5 @@
 #include "amsynth.h"
+#include "unistd.h"
 
 Addsynth::Addsynth(){
   cout << "Addsynth constructor" << endl;
@@ -40,18 +41,8 @@ int Addsynth::makeSound(float baseFrequency){
 
     jack.autoConnect();
 
-    //keep the program running and listen for user input, q = quit
-    std::cout << "\n\nPress 'q' when you want to quit the program.\n";
-    bool running = true;
-    while (running)
-    {
-      switch (std::cin.get())
-      {
-        case 'q':
-          running = false;
-          jack.end();
-          break;
-      }
-    }
+    usleep(1900000);
+    jack.end();
+
     return 0;
 }
--- cpp_basics/synth_song/melodygen.cpp
+++ cpp_basics/synth_song/melodygen.cpp
@@ -1,5 +1,5 @@
 #include "melodygen.h"
-
+#include "unistd.h"
 
 
 Melodygen::Melodygen() {
@@ -12,21 +12,26 @@ void Melodygen::AMplay(){
 
   Addsynth synht1;
 
-  random_device dev;
-  mt19937 rng(dev());
-  uniform_int_distribution<std::mt19937::result_type> dist(1,2);
-  float note;
-  switch (dist(rng)) {
-    case 1:
-      note = 220;
-      synht1.makeSound(220);
-      break;
-    case 2:
-      note = 440;
-      synht1.makeSound(440);
-      break;
-  }
+  Saw env2;
 
+  for (int i = 0; i < 3; i++){
+    cout << i << endl;
+    random_device dev;
+    mt19937 rng(dev());
+    uniform_int_distribution<std::mt19937::result_type> dist(1,2);
+    float note;
+    switch (dist(rng)) {
+      case 1:
+        note = 220;
+        synht1.makeSound(220);
+        break;
+      case 2:
+        note = 440;
+        synht1.makeSound(440);
+        break;
+    }
+    usleep(500000);
+  }
 }
 
 void Melodygen::Mixplay(){
