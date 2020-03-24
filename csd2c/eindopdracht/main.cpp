58a8c5a3e6abd52bfbc372a45de8c5d73bfede5e
Freekheldens <freekheldens@hotmail.com>
2020-03-24 13:27:45 +0100

    removed unwanted libraries

--- csd2c/eindopdracht/main.cpp
+++ csd2c/eindopdracht/main.cpp
@@ -6,7 +6,6 @@
 #include "sine.h"
 #include "lfo_sine.h"
 #include "distortion.h"
-#include "osc.h"
 #include "circBuffer.h"
 #include "flanger.h"
 
@@ -21,39 +20,6 @@ using namespace std;
  */
 
 #define PI_2 6.28318530717959
-
-// global variables, to be received via OSC
-float oscLfoSpeedR;
-float oscLfoSpeedL;
-float oscLfoDepth;
-float oscDrive;
-
-// subclass OSC into a local class so we can provide our own callback
-class localOSC : public OSC
-{
-  int realcallback(const char *path,const char *types,lo_arg **argv,int argc)
-  {
-  // osc "paramater" name
-  string msgpath=path;
-    // if osc "parameter" name is /oscLfoSpeedR
-    if(!msgpath.compare("/oscLfoSpeedR")){
-      oscLfoSpeedR = argv[0]->f;
-    }
-    // if osc "parameter" name is /oscLfoSpeedL
-    if(!msgpath.compare("/oscLfoSpeedL")){
-      oscLfoSpeedL = argv[0]->f;
-    }
-    // if osc "parameter" name is /oscLfoDepth
-    if(!msgpath.compare("/oscLfoDepth")){
-      oscLfoDepth = argv[0]->f;
-    }
-    // if osc "parameter" name is /oscDrive
-    if(!msgpath.compare("/oscDrive")){
-      oscDrive = argv[0]->f;
-    }
-    return 0;
-  }
-};
 
 int main(int argc,char **argv)
 {
@@ -80,16 +46,6 @@ int main(int argc,char **argv)
   // Flanger(samplerate,feedback,lfoFreq,lfoDepth,drywetmix)
   Flanger flanger1(samplerate, 80, 0.1, 50, 90);
 
-  // setting up OSC server
-  localOSC osc;
-  string serverport="10024";
-  osc.init(serverport);
-  osc.set_callback("/oscLfoSpeedR","f");
-  osc.set_callback("/oscLfoSpeedL","f");
-  osc.set_callback("/oscLfoDepth","f");
-  osc.set_callback("/oscDrive","f");
-  osc.start();
-  cout << "Listening on port " << serverport << endl;
 
   //assign a function to the JackModule::onProces
   jack.onProcess = [&](jack_default_audio_sample_t *inBuf,
@@ -102,14 +58,14 @@ int main(int argc,char **argv)
       lfoR.tick(samplerate);
 
       //variables received over OSC
-      lfoR.setFrequency(oscLfoSpeedR);
-      lfoL.setFrequency(oscLfoSpeedL);
+      lfoR.setFrequency(2);
+      lfoL.setFrequency(1);
 
       // left channel drive with lfo modulation
-      float driveL = ((lfoL.getSample() * oscLfoDepth) + oscDrive);
+      float driveL = ((lfoL.getSample() * 10) + 1);
 
       //right channel drive with lfo modulation
-      float driveR = ((lfoR.getSample() * oscLfoDepth) + oscDrive);
+      float driveR = ((lfoR.getSample() * 10) + 1);
 
       // distorting sine1 with distL and distR (this could also be inBuf[i] instead of sine)
       outBufL[i] = (distL.getSample((sine1.getSample() * driveL))) / driveL;
